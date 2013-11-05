250b4e5f3e8f86183922c2c989a80affd4dbca01
TingPing <TingPing@users.noreply.github.com>
2013-11-04 21:52:02 -0500

    Try out new theme

--- conf.py
+++ conf.py
@@ -107,6 +107,8 @@ html_theme = 'basicstrap'
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ['_theme']
 
+RTD_NEW_THEME = True
+
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 #html_title = 'HexChat project'
